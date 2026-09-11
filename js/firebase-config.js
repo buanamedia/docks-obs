e0b2cdc2593164988ae0c0b709dabb3d04e85640
buanamedia <media.agussiahaan@gmail.com>
2026-09-11 08:50:58 +0000

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,4 @@
-// Menggunakan Firebase Compat SDK agar mudah dipakai di HTML biasa
+// Konfigurasi Firebase dari Firebase Console
 const firebaseConfig = {
   apiKey: "AIzaSyC9r6c7zccoOtWaCctVUzebr1AsHlDSnrE",
   authDomain: "obs-docks-control.firebaseapp.com",
@@ -6,9 +6,13 @@ const firebaseConfig = {
   projectId: "obs-docks-control",
   storageBucket: "obs-docks-control.firebasestorage.app",
   messagingSenderId: "9188420341",
-  appId: "1:9188420341:web:8b2f4c42240fd7b45db3bc"
+  appId: "1:9188420341:web:8b2f4c42240fd7b45db3bc",
 };
 
-// Inisialisasi Firebase
-firebase.initializeApp(firebaseConfig);
+// Inisialisasi Firebase jika belum diinisialisasi
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Global database reference untuk digunakan di dock.html & overlay.html
 const database = firebase.database();
